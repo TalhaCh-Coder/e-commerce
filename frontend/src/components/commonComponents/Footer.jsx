ed1eaf62bcfa0e67248fcafd43f9c96ccea206c0
TalhaCh-Coder <talhaahmeduni022@gmail.com>
2026-01-15 13:20:00 +0500

    A responsive and beautiful footer is created

--- frontend/src/components/commonComponents/Footer.jsx
+++ frontend/src/components/commonComponents/Footer.jsx
@@ -1,7 +1,100 @@
+import { FaLinkedin, FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 function Footer() {
+  const quickLinks = [
+    { name: "Home", url: "/" },
+    { name: "Products", url: "/products" },
+    { name: "About Us", url: "/about" },
+    { name: "Contact", url: "/contact" },
+    { name: "FAQ", url: "/categories" },
+  ];
+
   return (
     <>
-      <h1>This is footer.</h1>
+      <footer className="relative ">
+        <div className="absolute bg-blue-700 w-11/12 sm:w-4/5 md:w-3/5 lg:w-3/5 -top-55 md:-top-40 lg:-top-43 left-1/2 transform -translate-x-1/2 shadow-xl rounded-2xl flex flex-col items-center px-6 py-8 md:py-10 lg:py-12">
+          <h2 className="text-white text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold text-center">
+            Subscribe to Our Newsletter
+          </h2>
+
+          <div className="mt-6 md:mt-10 flex flex-col md:flex-row w-full md:w-[90%] lg:w-[70%] gap-3 items-center md:border md:border-white md:rounded-lg overflow-hidden px-2 md:px-0">
+            <input
+              type="email"
+              placeholder="Enter your email..."
+              className="w-full sm:flex-1 md:w-[60%] py-3 px-4 text-lg md:text-xl placeholder-white text-white bg-transparent border border-white rounded-lg md:border-none focus:caret-white outline-none"
+            />
+            <button className="w-full bg-white md:w-[40%] text-blue-700 py-3 px-6 text-lg md:text-xl font-semibold rounded-lg hover:bg-gray-100 transition-colors duration-300">
+              Subscribe
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1  lg:grid-cols-3 bg-gray-200 pt-14">
+          <div className="flex items-center p-8 md:pl-24">
+            <p className="text-lg text-center md:text-left">
+              Welcome to{" "}
+              <span className="font-semibold text-blue-700 text-xl italic">
+                Ahmed’s Shop
+              </span>
+              , your one-stop online store for quality products at great prices.
+              Enjoy a secure shopping experience, fast delivery, and excellent
+              customer service every time you shop with us.
+            </p>
+          </div>
+          <div className="py-10 text-center">
+            <h2 className="font-semibold text-2xl pb-4">Locations</h2>
+            <div className=" flex flex-col gap-3 items-center">
+              <p className="cursor-pointer transition duration-300 hover:text-gray-500 w-fit">
+                New York
+              </p>
+              <p className="cursor-pointer transition duration-300 hover:text-gray-500 w-fit">
+                Los Angeles
+              </p>
+              <p className="cursor-pointer transition duration-300 hover:text-gray-500 w-fit">
+                Chicago
+              </p>
+              <p className="cursor-pointer transition duration-300 hover:text-gray-500 w-fit">
+                Houston
+              </p>
+              <p className="cursor-pointer transition duration-300 hover:text-gray-500 w-fit">
+                Phoenix
+              </p>
+            </div>
+          </div>
+          <div className="py-10 text-center">
+            <h2 className="font-semibold text-2xl pb-4">Quick Links</h2>
+            <div className=" flex flex-col gap-3 items-center">
+              {quickLinks.map((link) => (
+                <Link
+                  to={link.url}
+                  key={link.name}
+                  className="block hover:underline"
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row items-center gap-10 bg-gray-300 py-8 px-6 border-t border-gray-400 justify-between">
+          <div>
+            <p>@2026-Ahmed's Shop</p>
+          </div>
+          <div className="flex items-center justify-center  gap-3">
+            <FaLinkedin size={25} style={{ marginRight: "1rem" }} />
+            <FaFacebook size={25} style={{ marginRight: "1rem" }} />
+            <FaInstagram size={25} style={{ marginRight: "1rem" }} />
+            <FaTiktok size={25} style={{ marginRight: "1rem" }} />
+          </div>
+          <div className="flex items-center justify-center gap-3">
+            <p>Terms</p>
+            <p>Privacy</p>
+            <p>Cookies</p>
+            <p>Help</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,53 @@
 function HomePage() {
   return (
     <>
-      <h1>This is home page.</h1>
+      <h1>
+        This is home page. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Dolores sit exercitationem possimus obcaecati maiores perferendis
+        velit, dicta natus aspernatur, cum, sint facilis deleniti excepturi
+        molestiae sequi reiciendis laudantium dolorum doloribus unde culpa nobis
+        eum iusto suscipit. Voluptate, deserunt? Animi eum nihil ipsum harum!
+        Eaque nostrum aspernatur aut? Non expedita iusto quos, voluptates earum
+        ipsum illo tenetur commodi praesentium suscipit possimus molestiae eum
+        consectetur aliquam nisi culpa quae consequatur facilis reprehenderit
+        nam, aliquid placeat, totam alias eveniet. Non provident ipsa, iusto
+        ducimus explicabo veritatis consequatur quidem? Nostrum, numquam
+        facilis! Exercitationem amet, sint consequuntur consectetur mollitia
+        quis alias magnam autem esse iste cupiditate, ad culpa dicta nulla
+        velit? Consectetur amet saepe facere facilis repellendus illum, eligendi
+        non neque cumque repellat provident corrupti quis veniam quidem nesciunt
+        earum minus quaerat error sit explicabo officia rem eaque? Harum beatae,
+        quod deserunt explicabo recusandae tenetur ipsam consequatur neque
+        doloremque cum soluta, vitae error nesciunt rem illo doloribus quo atque
+        veritatis adipisci necessitatibus libero animi? Optio autem dolores
+        recusandae voluptatum sit laboriosam sed maxime quasi perspiciatis.
+        Maiores pariatur sequi aliquam, enim voluptatum delectus vel? Provident
+        illum commodi porro doloremque ipsam asperiores quibusdam id molestiae.
+        Inventore, possimus a aspernatur eveniet iusto ipsam laborum, voluptas
+        laboriosam tempora necessitatibus architecto dignissimos deleniti
+        nesciunt, nihil enim consequatur? Eius earum modi minus quam illo
+        consectetur inventore, rerum iste fuga similique assumenda perferendis?
+        Dolore quis perspiciatis perferendis laudantium quos. Iure qui error
+        dolorem, deserunt ducimus at vel tenetur, sit totam, quo porro ut
+        veniam! Sit, aliquid consequatur. Maiores culpa corporis cum sapiente
+        reiciendis recusandae quos deleniti iste. Ut repudiandae quisquam magnam
+        animi hic molestias id quibusdam fuga odit mollitia et, voluptates eos
+        sit totam reprehenderit soluta aut deserunt voluptate voluptatibus
+        voluptatum, rem officia. At eligendi voluptatem repellat laudantium?
+        Soluta voluptate accusamus blanditiis quia nobis error pariatur minima
+        atque quae, necessitatibus possimus mollitia, ipsum molestias? Magnam
+        sit iste impedit commodi nemo culpa, dolore debitis illo, esse
+        laudantium repudiandae accusantium dolores, iure excepturi quos qui.
+        Temporibus modi reprehenderit doloribus cupiditate reiciendis recusandae
+        repellendus obcaecati, quas, debitis a est in eveniet odit accusamus
+        animi beatae officiis iusto sequi, cumque voluptates cum repudiandae
+        molestias. Animi in accusantium voluptate nihil nostrum quae, voluptas
+        fuga hic perferendis, et vitae molestiae est voluptatem vel placeat
+        fugit nulla ullam nobis magnam. Ea, autem. Est a enim harum asperiores
+        et rem numquam, suscipit dolorem repellendus officia dolorum veritatis
+        quam illo nemo quibusdam quae temporibus provident. Quae laboriosam
+        totam quia numquam? Animi?
+      </h1>
     </>
   );
 }
